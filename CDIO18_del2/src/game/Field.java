aa2cc743c18a88f22e08f193b19b83be821938c8
Helen <Helen@192.168.0.15>
2016-11-19 15:02:50 +0100

    Opdatering af field og oprettelse af Ownable

--- CDIO18_del2/src/game/Field.java
+++ CDIO18_del2/src/game/Field.java
@@ -1,31 +1,16 @@
 package game;
 
-import desktop_resources.GUI;
-
 public class Field 
 {
-//	private int no = 0;
-	private int value = 0;
 	private String name = "None";
 	private String description = "";
-	private boolean extraTurn = false;
 	
-	public Field(int value, String name, String description, boolean extraTurn){
-		this( value, name, description);
-		this.extraTurn = extraTurn;
-	}
-	public Field(int value, String name, String description)
+	public Field(String name, String description)
 	{
-		this.value = value;
 		this.name = name;
 		this.description = description;
 	}
-	
-	public int getValue()
-	{
-		return value;
-	}
-	
+
 	public String getName()
 	{
 		return name;
@@ -35,20 +20,10 @@ public class Field
 	{
 		return description;
 	}
-	
-	public boolean landOn(Player p, int no){
-		GUI.setCar(no,p.getName());
-		GUI.showMessage(getDescription());
-		p.getAccount().addSum(getValue());
 
-		System.out.println("Flyttet til felt: " + no);
-		return extraTurn;
-	}
-	
 	public String toString()
 	{
-		return 	"Value: " + value + "\n" + 
-				"Name: " + name + "\n" + 
+		return 	"Name: " + name + "\n" + 
 				"Description: " + description;
 	}
 }
