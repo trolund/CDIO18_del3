e445ea0d8a6517b73c022b65778785f7c74494ac
Kasper Leiszner <leisznern@gmail.com>
2016-11-22 11:41:27 +0100

    Somefixes

--- CDIO18_del2/src/game/Tax.java
+++ CDIO18_del2/src/game/Tax.java
@@ -19,9 +19,9 @@ public class Tax extends Field {
 		this.taxRate = taxRate;
 	}
 
-	public void landOn(Player p, int no, Output out){
-	always(p,no,out);
-	
+	public void landOn(Player p, int no, Output out)
+	{
+		always(p,no,out);
 		payTax(p);
 	}
 
@@ -29,14 +29,13 @@ public class Tax extends Field {
 	{
 		if(p.getCarPos() == 17)
 		{
-
 			if(out.taxAction(taxAmmount))
 			{
 				p.getAccount().withdraw(taxAmmount);
 			}
 			else
 			{
-				 p.getAccount().withdraw( (int) (p.getAccount().getSum() * ((100-taxRate)/100)));
+				p.getAccount().withdraw( (int) (p.getAccount().getSum() * ((100-taxRate)/100)));
 			}
 
 		}
