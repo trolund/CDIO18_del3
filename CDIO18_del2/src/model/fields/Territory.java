df62df6f97aee2d75070b09b494f5ce5b6bdbbcd
troelslund <troelslund@192.168.1.46>
2016-11-25 10:29:13 +0100

    Ud med tekst

--- CDIO18_del2/src/model/fields/Territory.java
+++ CDIO18_del2/src/model/fields/Territory.java
@@ -35,20 +35,17 @@ public class Territory extends Ownable
 		{
 			// cant affort
 			Output.deniedPurchase();
-			System.out.println("You can't affort");
 		}
 		else if(p.getAccount().getSum()>=price && getOwner() == null && !wantToBuy)
 		{
 			//Player don't want to buy
 			Output.deniedPurchase();
-			System.out.println("You didn't buy the field");
 		}
 		else if(getOwner() != null && getOwner() != p)// is owned
 		{
 			// Pay rent
 			p.getAccount().withdraw(getRent(getOwner()));
 			getOwner().getAccount().addSum(getRent(getOwner()));
-			System.out.println("Field is owned, you paid the rent");
 			Output.payedRent(p, getRent(getOwner()));
 		}
 		else
